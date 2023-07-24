4747c1eb388bee2fb7829bcffcaca86e6707a523
gitmquick <ussmwq@gmail.com>
2023-07-24 04:09:00 -0400

    Tabled Tunneler

--- 10/psu_tunnel.lua
+++ 10/psu_tunnel.lua
@@ -19,20 +19,28 @@ end
 local function loadTurtleInfo()
     local file = fs.open(saveFile, "r")
     local data = split(file.readLine(),",")
-    local location, heading, script = data[1], data[2], data[3]
-    -- local location = data[1]
-    -- local heading = data[2]
-    -- local script = data[3]
+    local location, heading, script, exeIndex = data[1], data[2], data[3], data[4]
     file.close()
-    return location, heading, script
+    return location, heading, script, exeIndex
 end
 
--- Unique Logic
+--==============================================
+--==============================================
+--==============================================
+--==============================================
+--==============================================
+--Personally Unique Logic
+--==============================================
+--==============================================
+--==============================================
+--==============================================
+--==============================================
+
 f_alc = fs.open(saveFile, "w") --Filesystem alignment location control (VarName)
-lastLocation, heading, lastScript = loadTurtleInfo()
+lastLocation, heading, lastScript, exeIndex = loadTurtleInfo()
 
 local function writeALC()
-    local data = location .. "," .. heading .. "," .. script
+    local data = location .. "," .. heading .. "," .. script .. "," .. exeIndex
     file.writeLine(data)
 end
 
@@ -79,12 +87,74 @@ local function turnRight()
 end
 
 --=======================
+--=======================
+--=======================
+--=======================
+--=======================
+--Script Restart Handler
+--=======================
+--=======================
+--=======================
+--=======================
+--=======================
+
+--Any script written with the concept of a restartable script must have ways to be restarted.
+
+--=======================
+--=======================
+--=======================
+--=======================
+--=======================
 --Turtle Movement Logic
 --=======================
+--=======================
+--=======================
+--=======================
+--=======================
+
+local function refuel()
+    local fuelLevel = turtle.getFuelLevel()
+    if fuelLevel == "unlimited" or fuelLevel > 0 then
+        return
+    end
+
+    local function tryRefuel()
+        for n = 1, 16 do
+            if turtle.getItemCount(n) > 0 then
+                turtle.select(n)
+                if turtle.refuel(1) then
+                    turtle.select(1)
+                    return true
+                end
+            end
+        end
+        turtle.select(1)
+        return false
+    end
+
+    if not tryRefuel() then
+        print("Add more fuel to continue.")
+        while not tryRefuel() do
+            os.pullEvent("turtle_inventory")
+        end
+        print("Resuming Tunnel.")
+    end
+end
 
 local function breakUp()
     while turtle.detectUp() do
         if turtle.digUp() then
+            sleep(0.5)
+        else
+            return false
+        end
+    end
+    return true
+end
+
+local function breakDown()
+    while turtle.detectDown() do
+        if turtle.digDown() then
             collect()
             sleep(0.5)
         else
@@ -97,7 +167,6 @@ end
 local function breakF()
     while turtle.detect() do
         if turtle.dig() then
-            collect()
             sleep(0.5)
         else
             return false
@@ -110,11 +179,11 @@ local function moveUp()
     refuel()
     while not turtle.up() do
         if turtle.detectUp() then
-            if not tryDigUp() then
+            if not breakUp() then
                 return false
             end
         elseif turtle.attackUp() then
-            collect()
+            sleep(0.5)
         else
             sleep(0.5)
         end
@@ -126,11 +195,11 @@ local function moveDown()
     refuel()
     while not turtle.down() do
         if turtle.detectDown() then
-            if not tryDigDown() then
+            if not breakDown() then
                 return false
             end
         elseif turtle.attackDown() then
-            collect()
+            sleep(0.5)
         else
             sleep(0.5)
         end
@@ -142,11 +211,11 @@ local function moveForward()
     refuel()
     while not turtle.forward() do
         if turtle.detect() then
-            if not tryDig() then
+            if not breakF() then
                 return false
             end
         elseif turtle.attack() then
-            collect()
+            sleep(0.5)
         else
             sleep(0.5)
         end
@@ -166,21 +235,33 @@ end
 --=====================--
 --=====================--
 
+--Functions that would normally be the driver
+local mainFuncTab = {      
+    turtle.placeDown,
+    breakUp,
+    turnLeft,
+    breakF,
+    moveUp,
+    breakF,
+    turnRight,
+    turnRight,
+    breakF,
+    moveDown,
+    breakF,
+    turnLeft
+}
+
+--Handle nils, files don't always have the required data
+if (not exeIndex)
+    exeIndex = 0
+end
+
 for n = 1, length do
-    turtle.placeDown()
-    breakUp()
-    turnLeft()
-    breakF()
-    moveUp()
-    writeALC()
-    breakF()
-    turnRight()
-    turnRight()
-    breakF()
-    moveDown()
-    writeALC()
-    breakF()
-    turnLeft()
+    for i = exeIndex + 1, #mainFuncTab do
+        mainFuncTab[i]()
+        exeIndex = i
+        writeALC()
+    end
 
     if n < length then
         breakF()
@@ -188,6 +269,7 @@ for n = 1, length do
             print("Aborting Tunnel.")
             break
         end
+        writeALC()
     else
         print("Tunnel complete.")
     end
